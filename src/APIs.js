5d43d57c94f715ecf6294ea5103cd4c714deb35c
Muhammad Saad <muhammadsaad28102003@gmail.com>
2023-11-11 14:29:59 +0500

    Update APIs.js

--- src/APIs.js
+++ src/APIs.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:2200"
+export const host = "https://chat-app-server-production-6b2d.up.railway.app"
 export const registerAPI = `${host}/api/auth/register`
 export const loginAPI = `${host}/api/auth/login`
 export const allUsersAPI = `${host}/api/auth/allusers`
